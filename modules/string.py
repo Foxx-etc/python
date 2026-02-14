be6f4f86fc7aec094bb9b6edf15b25ef7a7e5412
Foxx-etc <charlieadya2007@gmail.com>
2026-02-14 22:28:53 +0530

    Update string.py

--- modules/string.py
+++ modules/string.py
@@ -1,5 +1,5 @@
 # Removes The Space In String And Joins The Characters
-def drop_space(s):
+def drop_space(s) -> str:
 	new_str = ""
 	for i in s:
 		if i==" ":
@@ -8,8 +8,9 @@ def drop_space(s):
 			new_str += i
 	return new_str
 
+
 # Return True If Spaces In String
-def is_space(s):
+def is_space(s) -> bool:
 	for i in s:
 		if i==" ":
 			return True
@@ -17,8 +18,9 @@ def is_space(s):
 			continue
 	return False
 
+
 # Counts The Number Of String In Iterables and Return Int value
-def count_str(s):
+def count_str(s) -> int:
 	number=0
 	for i in s:
 		if type(i) is str:
@@ -28,7 +30,7 @@ def count_str(s):
 	return int(number)
 
 # Counts The Nunber Of Integer Values In Iterables and Return Int value
-def count_int(n):
+def count_int(n) -> int:
 	number=0
 	for i in n:
 		if type(i) is int:
@@ -37,27 +39,5 @@ def count_int(n):
 			continue
 	return int(number)
 
-
-# Removes The Negative Integer & Ignores the String
-def drop_neg_int(l):
-	pos = []	
-	for i in l:
-		if type(i) is str or i < 0:
-			continue	
-		else:
-			pos.append(i)	
-	return pos
-
-
-
-# Removes The Positive Integer & Ignores the String
-def drop_pos_int(l):
-	neg = []
-	for i in l:
-		if type(i) == str or i > 0:
-			continue
-		else:
-			neg.append(i)
-	return neg
 
 
