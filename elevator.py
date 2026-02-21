c436aba82a808582b967cd63a107cbc63cd7df00
Foxx-etc <charlieadya2007@gmail.com>
2026-02-22 00:40:14 +0530

    Update elevator.py

--- elevator.py
+++ elevator.py
@@ -2,12 +2,12 @@
 # TODO : much improvement has to brought such as parallel user prompt 
 #        validation, etc.
 
-
 from time import sleep
 from os import system
 system("clear")
 
 idle_at_floor = 0
+
 
 #will display upcoming, downcoming or already at floor
 def req_at_floor(n):
@@ -15,31 +15,34 @@ def req_at_floor(n):
     
     global idle_at_floor
     if n == idle_at_floor:
-        print("Already At The Floor\n")
-    elif n<0 or n>=10:
+        print("Elevator Already At The Floor!\n")
+    elif n < 0 or n >= 10:
         print("Out of floor!\n")
     else:
         while idle_at_floor != n:
             sleep(1)
+            
 
             #upcoming
             if n >= idle_at_floor:
                 idle_at_floor += 1
-                print("\u2191", idle_at_floor)
+                print("\u2191" , idle_at_floor)
                 if n == idle_at_floor:
-                    print("opening the door...")
+                    print("🡸  🡺")
                     sleep(2)
-                    print("closing the door...\n")
-                    
+                    print("🡺  🡸\n")
+            
 
             #downcoming
             elif n <= idle_at_floor:
                 idle_at_floor -= 1
-                print("\u2193", idle_at_floor)
+                print("\u2193" , idle_at_floor)
                 if n == idle_at_floor:
-                    print("opening the door...")
+                    print("🡸  🡺")
                     sleep(2)
-                    print("closing the door...\n")
+                    print("🡺  🡸\n")
+  
+
 
 while True:
     user = int(input("Enter Floor Number : "))
