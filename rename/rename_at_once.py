1301d059ddb93d48baead9ece67157db6055c5e0
CARNIVAL <charlieadya2007@gmail.com>
2026-03-16 00:33:23 +0530

    Create rename_at_once.py

---
+++ rename/rename_at_once.py
@@ -0,0 +1,31 @@
+import holi , os , pathlib as p
+
+num = 1
+parent_dir = "/sdcard/just/"
+
+print(holi.lcyan)
+print("This Program Rename All The Unnamed Files At Once")
+print(holi.lred)
+print("This Program Does Not Reserve The filename That were Already Renamed, In Short This Bad..")
+print(holi.lgreen)
+print("For A Perfect One Use Another Script Programmed By Me")
+print("It's Named", holi.lmagenta, "rename_at_once_safer.py")
+print(holi.reset)
+print(f'You Are In {parent_dir} Set The Name To All File At Once. All The File Will Have Same Name With Distinct Number') 
+print()
+rename = input("Enter File Name : ")
+
+
+for file in os.listdir(parent_dir):
+	
+	stem = p.Path(file).stem
+	suffix = p.Path(file).suffix
+	
+	old = parent_dir + file
+	new_num_file = parent_dir + rename + str(num) + suffix
+	
+	os.rename(old , new_num_file)
+	num += 1
+	
+
+
